81bdf28a133d3bafcbb8855955fa5fd17ea33031
raj-adhikarii <rajadhikari777.ra@gmail.com>
2023-04-24 11:06:46 +0545

    Stire URL fixed

--- header.php
+++ header.php
@@ -121,7 +121,7 @@
                                                 ?>
 
                                             <li><a class="dropdown-item" href="<?php echo $user_profile_link; ?>?type=favorite/"><i class="far fa-heart"></i> My Favorites</a></li>
-                                            <li><a class="dropdown-item" href="<?php echo site_url(); ?>/dashboard/settings/stor/e"><i class="far fa-cog"></i> Settings</a></li>
+                                            <li><a class="dropdown-item" href="<?php echo site_url(); ?>/dashboard/settings/store"><i class="far fa-cog"></i> Settings</a></li>
                                             <li><a class="dropdown-item" href="<?php echo wp_logout_url( home_url() ); ?>"><i class="far fa-sign-out"></i> Log Out</a></li>
                                         </ul>
                                     <?php else : ?>
