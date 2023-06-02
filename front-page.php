20be360cc810e8a746f68aadef91500ca4f0d49f
raj-adhikarii <rajadhikari777.ra@gmail.com>
2023-06-02 16:37:45 +0545

    mobile login btn added

--- front-page.php
+++ front-page.php
@@ -42,7 +42,7 @@ get_header();
                                 <?php endif; ?>
                             <div class="hero-btn">
                                 <a href="<?php echo site_url(); ?>/shop/" class="theme-btn">Browse Ads <i class="fas fa-arrow-right"></i></a>
-                                <a href="#" class="theme-border-btn text-white">Post Your Ads <i
+                                <a href="<?php echo site_url(); ?>/dashboard/" class="theme-border-btn text-white">Post Your Ads <i
                                         class="fas fa-arrow-right"></i></a>
                             </div>
                         </div>
@@ -377,53 +377,6 @@ get_header();
     </div>
 </div>
 <!-- category area end -->
-
-
-
-<!-- location area -->
-<div class="location-area py-120">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-7 mx-auto wow fadeInDown" data-wow-duration="1s" data-wow-delay=".25s">
-                <div class="site-heading text-center">
-                    <span class="site-title-tagline">Location</span>
-                    <h2 class="section-title">Most Popular Location</h2>
-                    <p>It is a long established fact that a reader will be distracted by the readable content.
-                    </p>
-                </div>
-            </div>
-        </div>
-        <?php
-                $args = array(
-                    'post_type' => 'location',
-                    'posts_per_page' => 6
-                );
-
-                $query = new WP_Query($args); ?>
-
-                <?php if($query->have_posts()) : ?>
-                <div class="row align-items-center">
-                    <?php while($query->have_posts()): $query->the_post(); ?> 
-                    <div class="col-md-12 col-lg-4">
-                        <a href="<?php the_permalink(); ?>" class="location-item wow fadeInUp" data-wow-duration="1s" data-wow-delay=".25s">
-                            <div class="location-img">
-                                <img src="https://images.unsplash.com/photo-1588992370249-1b0fcaf6249b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="">
-                            </div>
-                            <div class="location-info">
-                                <h4>New York <?php the_title(); ?></h4>
-                                <span><i class="far fa-location-dot"></i> 30 Ads</span>
-                            </div>
-                        </a>
-                    </div>
-                    <?php endwhile; ?>
-                </div>
-                <?php wp_reset_postdata(); ?>
-                <?php endif; ?>
-    </div>
-</div>
-<!-- location area end -->
-
-
 
 <!-- counter area -->
 <div class="counter-area bg pt-60 pb-60">
