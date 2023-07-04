55b534bdc7c3dbd2ee2e19a0e9dbe43c7167307b
raj-adhikarii <rajadhikari777.ra@gmail.com>
2023-07-04 12:02:18 +0545

    userid added to product

--- inc/api.php
+++ inc/api.php
@@ -1,97 +1,8 @@
 <?php 
-/*===================================/*
-    loggedin server site validation
-/*===================================*/
-if ( $_SERVER['REQUEST_METHOD'] === 'POST' && isset( $_POST['login'] ) ) {
-    // Verify the nonce for security
-    if ( isset( $_POST['custom_login_nonce'] ) && wp_verify_nonce( $_POST['custom_login_nonce'], 'custom_login_nonce' ) ) {
-        $username = sanitize_text_field( $_POST['username'] );
-        $password = $_POST['password'];
-
-        // Perform your custom authentication logic here
-        $user = wp_authenticate( $username, $password );
-
-        if ( ! is_wp_error( $user ) ) {
-            // Successful login
-            wp_set_auth_cookie( $user->ID );
-            wp_redirect( site_url( '/dashboard/' ) ); // Redirect to home page after login
-            exit;
-        } else {
-            // Login failed
-            $error_message = $user->get_error_message();
-            // Display the error message or perform any other desired action
-            echo 'Login failed: ' . $error_message;
-        }
-    } else {
-        // Invalid nonce
-        echo 'Security check failed. Please try again.';
-    }
-}
-
-/*======================/*
-    signup validation
-/*======================*/
-if ( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
-    $username = sanitize_user( $_POST['username'] );
-    $email = sanitize_email( $_POST['email'] );
-    $password = $_POST['password'];
-    $agree = isset( $_POST['agree'] ) ? true : false;
-
-    // Perform validation and registration logic here
-    $errors = array();
-
-    // Check if required fields are filled
-    if ( empty( $username ) ) {
-        $errors[] = 'Please enter your name.';
-    }
-
-    if ( empty( $email ) ) {
-        $errors[] = 'Please enter your email address.';
-    } elseif ( ! filter_var( $email, FILTER_VALIDATE_EMAIL ) ) {
-        $errors[] = 'Invalid email address.';
-    }
-
-    if ( empty( $password ) ) {
-        $errors[] = 'Please enter a password.';
-    }
-
-    // if ( ! $agree ) {
-    //     $errors[] = 'You must agree to the Terms of Service.';
-    // }
-
-    if ( empty( $errors ) ) {
-        $user_id = wp_create_user( $username, $password, $email );
-
-        if ( ! is_wp_error( $user_id ) ) {
-            // Registration successful
-            // You can perform additional actions here, such as sending a confirmation email, redirecting the user, etc.
-            wp_redirect( site_url( '/login/' ) );
-            exit;
-        } else {
-            $errors[] = $user_id->get_error_message();
-        }
-    }
-}
-
-/*==================================================== /*
-    Server-side validation for forget password page 
-/*====================================================*/
-function custom_lost_password_validation($errors, $username) {
-    if (empty($username)) {
-        $errors->add('empty_username', __('Please enter your email address.', 'woocommerce'));
-    } elseif (!is_email($username)) {
-        $errors->add('invalid_username', __('Invalid email address.', 'woocommerce'));
-    } elseif (!email_exists($username)) {
-        $errors->add('email_not_found', __('The provided email address is not registered.', 'woocommerce'));
-    }
-
-    return $errors;
-}
-add_filter('woocommerce_lostpassword_post_errors', 'custom_lost_password_validation', 10, 2);
-
 /*==========================/*
-    custom login endpoint
+   custom login endpoint
 /*==========================*/
+
 add_action('rest_api_init', 'custom_user_login_endpoint');
 
 function custom_user_login_endpoint() {
@@ -142,8 +53,7 @@ function custom_user_registration($request) {
   
     if (is_wp_error($user_id)) {
       return new WP_REST_Response(array('error' => $user_id->get_error_message()), 500);
-    }
-  
+    } 
     return new WP_REST_Response(array('message' => 'User registered successfully'), 200);
   }
   
@@ -266,7 +176,7 @@ function create_product_via_api($product_data) {
    retrieve a product along with the user ID of the user who published it
 /*========================================================================*/
 function get_product_with_user($request) {
-    $product_id = $request['product_id'];
+    $product_id = $request->get_param('product_id');
 
     // Get the product object
     $product = wc_get_product($product_id);
--- inc/custom-function.php
+++ inc/custom-function.php
@@ -1,4 +1,95 @@
 <?php
+
+/*===================================/*
+    loggedin server site validation
+/*===================================*/
+if ( $_SERVER['REQUEST_METHOD'] === 'POST' && isset( $_POST['login'] ) ) {
+    // Verify the nonce for security
+    if ( isset( $_POST['custom_login_nonce'] ) && wp_verify_nonce( $_POST['custom_login_nonce'], 'custom_login_nonce' ) ) {
+        $username = sanitize_text_field( $_POST['username'] );
+        $password = $_POST['password'];
+
+        // Perform your custom authentication logic here
+        $user = wp_authenticate( $username, $password );
+
+        if ( ! is_wp_error( $user ) ) {
+            // Successful login
+            wp_set_auth_cookie( $user->ID );
+            wp_redirect( site_url( '/dashboard/' ) ); // Redirect to home page after login
+            exit;
+        } else {
+            // Login failed
+            $error_message = $user->get_error_message();
+            // Display the error message or perform any other desired action
+            echo 'Login failed: ' . $error_message;
+        }
+    } else {
+        // Invalid nonce
+        echo 'Security check failed. Please try again.';
+    }
+}
+
+/*======================/*
+    signup validation
+/*======================*/
+if ( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
+    $username = sanitize_user( $_POST['username'] );
+    $email = sanitize_email( $_POST['email'] );
+    $password = $_POST['password'];
+    $agree = isset( $_POST['agree'] ) ? true : false;
+
+    // Perform validation and registration logic here
+    $errors = array();
+
+    // Check if required fields are filled
+    if ( empty( $username ) ) {
+        $errors[] = 'Please enter your name.';
+    }
+
+    if ( empty( $email ) ) {
+        $errors[] = 'Please enter your email address.';
+    } elseif ( ! filter_var( $email, FILTER_VALIDATE_EMAIL ) ) {
+        $errors[] = 'Invalid email address.';
+    }
+
+    if ( empty( $password ) ) {
+        $errors[] = 'Please enter a password.';
+    }
+
+    // if ( ! $agree ) {
+    //     $errors[] = 'You must agree to the Terms of Service.';
+    // }
+
+    if ( empty( $errors ) ) {
+        $user_id = wp_create_user( $username, $password, $email );
+
+        if ( ! is_wp_error( $user_id ) ) {
+            // Registration successful
+            // You can perform additional actions here, such as sending a confirmation email, redirecting the user, etc.
+            wp_redirect( site_url( '/login/' ) );
+            exit;
+        } else {
+            $errors[] = $user_id->get_error_message();
+        }
+    }
+}
+
+/*==================================================== /*
+    Server-side validation for forget password page 
+/*====================================================*/
+function custom_lost_password_validation($errors, $username) {
+    if (empty($username)) {
+        $errors->add('empty_username', __('Please enter your email address.', 'woocommerce'));
+    } elseif (!is_email($username)) {
+        $errors->add('invalid_username', __('Invalid email address.', 'woocommerce'));
+    } elseif (!email_exists($username)) {
+        $errors->add('email_not_found', __('The provided email address is not registered.', 'woocommerce'));
+    }
+
+    return $errors;
+}
+add_filter('woocommerce_lostpassword_post_errors', 'custom_lost_password_validation', 10, 2);
+
 /*===============================/*
  	Update product views count
 /*===============================*/
