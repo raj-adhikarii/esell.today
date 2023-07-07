a200829210418e180bb700ea3df944420fc66eb1
raj-adhikarii <rajadhikari777.ra@gmail.com>
2023-07-07 15:20:58 +0545

    custom password reset

--- auth/page-password-reset.php
+++ auth/page-password-reset.php
@@ -1,37 +1,58 @@
-<?php 
-// Template Name: Password Reset
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+<?php
+/*
+ * Template Name: Password Reset
+ */
+
+get_header();
+
+if (isset($_GET['key']) && isset($_GET['login'])) {
+    $user_login = $_GET['login'];
+    $reset_key = $_GET['key'];
+
+    $user = check_password_reset_key($reset_key, $user_login);
+
+    if (!is_wp_error($user)) {
+        if (isset($_POST['password_reset'])) {
+            $new_password = $_POST['new_password'];
+            $confirm_password = $_POST['confirm_password'];
+
+            if ($new_password === $confirm_password) {
+                reset_password($user, $new_password);
+                wc_add_notice(__('Your password has been reset successfully!', 'woocommerce'), 'success');
+            } else {
+                wc_add_notice(__('New password and confirm password do not match.', 'woocommerce'), 'error');
+            }
+        }
+    } else {
+        wc_add_notice(__('Invalid password reset key.', 'woocommerce'), 'error');
+    }
 }
+?>
 
-get_header(); ?>
+<div id="primary" class="content-area">
+    <main id="main" class="site-main">
 
-<main class="main">
-    <!-- breadcrumb -->
-    <?php if(has_post_thumbnail($post ->ID)):
-            $image = wp_get_attachment_image_src(get_post_thumbnail_id($post -> ID), 'single-post-thumbnail'); ?>
-        <div class="site-breadcrumb" style="background: url(<?php echo $image[0]; ?>)">
-            <div class="container">
-                <h2 class="breadcrumb-title"><?php echo get_the_title(); ?></h2>
-                <ul class="breadcrumb-menu">
-                    <li><a href="<?php echo site_url(); ?>">Home</a></li>
-                    <li class="active"><?php echo get_the_title(); ?></li>
-                </ul>
-            </div>
+        <div class="woocommerce">
+            <?php wc_print_notices(); ?>
+
+            <form method="post" class="woocommerce-form woocommerce-form-reset-password">
+                <p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+                    <label for="new_password"><?php esc_html_e('New Password', 'woocommerce'); ?><span class="required">*</span></label>
+                    <input type="password" class="woocommerce-Input woocommerce-Input--text input-text" name="new_password" id="new_password" required />
+                </p>
+                <p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+                    <label for="confirm_password"><?php esc_html_e('Confirm Password', 'woocommerce'); ?><span class="required">*</span></label>
+                    <input type="password" class="woocommerce-Input woocommerce-Input--text input-text" name="confirm_password" id="confirm_password" required />
+                </p>
+                <input type="hidden" name="password_reset" value="true" />
+                <?php wp_nonce_field('password-reset', 'password-reset-nonce'); ?>
+                <p class="form-row">
+                    <button type="submit" class="woocommerce-Button button" name="reset_submit"><?php esc_html_e('Reset Password', 'woocommerce'); ?></button>
+                </p>
+            </form>
         </div>
-    <?php endif; ?>
 
+    </main><!-- #main -->
+</div><!-- #primary -->
 
-    <!-- forgot password -->
-    <div class="login-area py-120">
-        <div class="container">
-            <div class="col-md-5 mx-auto">
-                <?php echo do_shortcode('[custom_woocommerce_login]'); ?>
-            </div>
-        </div>
-    </div>
-    <!-- forgot password end -->
-
-</main>
-
-<?php get_footer(); 
+<?php get_footer(); ?>
