0d008be390acb7107eac8a436448f1d05f0859ed
raj-adhikarii <rajadhikari777.ra@gmail.com>
2023-07-07 16:12:55 +0545

    store image upload btn edited

--- inc/custom-function.php
+++ inc/custom-function.php
@@ -447,7 +447,7 @@ function add_custom_fields_to_edit_store_information() {
 							<?php endif; ?>
 						</div>
 						<input type="file" class="woocommerce-Input woocommerce-Input--file input-file theme-btn store-upload" id="profile-image-upload profile-image-upload-button" name="store_logo">
-						<button type="button" class="theme-btn store-upload" id="profile-image-upload-button"><span class="far fa-upload"></span> Upload Profile Image</button>
+						<!-- <button type="button" class="theme-btn store-upload" id="profile-image-upload-button"><span class="far fa-upload"></span> Upload Profile Image</button> -->
 					</div>
 
 					<script>
