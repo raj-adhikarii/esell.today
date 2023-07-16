b0ea6c7e5a4dc402f19842d63d184a7b22c8bbb6
raj-adhikarii <rajadhikari777.ra@gmail.com>
2023-07-16 12:14:08 +0545

    wishlist

--- front-page.php
+++ front-page.php
@@ -371,89 +371,88 @@ get_header();
         </div>
 
         <div class="row">
-    <?php
-    $args = array(
-        'post_type'      => 'product',
-        'posts_per_page' => 8,
-        'orderby'        => 'date',
-        'order'          => 'DESC',
-    );
+            <?php
+            $args = array(
+                'post_type'      => 'product',
+                'posts_per_page' => 8,
+                'orderby'        => 'date',
+                'order'          => 'DESC',
+            );
 
-    $products = new WP_Query($args);
+            $products = new WP_Query($args);
 
-    if ($products->have_posts()) :
-        while ($products->have_posts()) : $products->the_post();
-            global $product;
-            $product_id = $product->get_id();
-            $product_name = $product->get_name();
-            $product_price = $product->get_price();
-            $product_sku = $product->get_sku();
-            $product_image_url = wp_get_attachment_image_src(get_post_thumbnail_id($product_id), 'medium')[0];
-            ?>
+            if ($products->have_posts()) :
+                while ($products->have_posts()) : $products->the_post();
+                    global $product;
+                    $product_id = $product->get_id();
+                    $product_name = $product->get_name();
+                    $product_price = $product->get_price();
+                    $product_sku = $product->get_sku();
+                    $product_image_url = wp_get_attachment_image_src(get_post_thumbnail_id($product_id), 'medium')[0];
+                    ?>
 
-            <div class="col-md-6 col-lg-3">
-                <div class="product-item wow fadeInUp" data-wow-duration="1s" data-wow-delay=".25s">
-                    <div class="product-img">
-                        <a href="<?php the_permalink(); ?>">
-                            <span class="product-status trending"><i class="fas fa-bolt-lightning"></i></span>
-                            <?php if ($product_image_url) : ?>
-                                <img src="<?php echo esc_url($product_image_url); ?>" alt="<?php echo esc_attr($product_name); ?>">
-                            <?php endif; ?>
-                            <a href="#" class="product-favorite" data-product-id="<?php echo get_the_ID(); ?>" data-product-name="<?php echo esc_attr($product_name); ?>"><i class="far fa-heart"></i></a>
-                        </a>
-                    </div>
-                    <div class="product-content">
-                        <div class="product-top">
-                            <div class="product-category">
-                                <div class="product-category-icon">
-                                    <i class="far fa-tv"></i>
+                    <div class="col-md-6 col-lg-3">
+                        <div class="product-item wow fadeInUp" data-wow-duration="1s" data-wow-delay=".25s">
+                            <div class="product-img">
+                                <a href="<?php the_permalink(); ?>">
+                                    <span class="product-status trending"><i class="fas fa-bolt-lightning"></i></span>
+                                    <?php if ($product_image_url) : ?>
+                                        <img src="<?php echo esc_url($product_image_url); ?>" alt="<?php echo esc_attr($product_name); ?>">
+                                    <?php endif; ?>
+                                    <a href="#" class="product-favorite" data-product-id="<?php echo get_the_ID(); ?>" data-product-name="<?php echo esc_attr($product_name); ?>"><i class="far fa-heart"></i></a>
+                                </a>
+                            </div>
+                            <div class="product-content">
+                                <div class="product-top">
+                                    <div class="product-category">
+                                        <div class="product-category-icon">
+                                            <i class="far fa-tv"></i>
+                                        </div>
+                                        <h6 class="product-category-title"><a href="#"><?php echo $product->get_categories(); ?></a></h6>
+                                    </div>
+                                    <div class="product-rate">
+                                        <i class="fas fa-star"></i>
+                                        <span>4.5</span>
+                                    </div>
                                 </div>
-                                <h6 class="product-category-title"><a href="#"><?php echo $product->get_categories(); ?></a></h6>
-                            </div>
-                            <div class="product-rate">
-                                <i class="fas fa-star"></i>
-                                <span>4.5</span>
-                            </div>
-                        </div>
-                        <div class="product-info">
-                            <h5><a href="<?php the_permalink(); ?>"><?php echo $product_name; ?></a></h5>
-                            <?php
-                            $posted_time = get_the_time('U'); // get the post published time
-                            $current_time = current_time('timestamp'); // get the current time
+                                <div class="product-info">
+                                    <h5><a href="<?php the_permalink(); ?>"><?php echo $product_name; ?></a></h5>
+                                    <?php
+                                    $posted_time = get_the_time('U'); // get the post published time
+                                    $current_time = current_time('timestamp'); // get the current time
 
-                            $time_diff = human_time_diff($posted_time, $current_time); // get the time elapsed since post was published
+                                    $time_diff = human_time_diff($posted_time, $current_time); // get the time elapsed since post was published
 
-                            echo ' <div class="product-date"><i class="far fa-clock"></i> ' . $time_diff . ' ago' . ' </div>'; // display the time elapsed
-                            ?>
-                        </div>
-                        <div class="product-bottom">
-                            <div class="product-price"><?php echo wc_price($product_price); ?></div>
-                            <a href="<?php the_permalink(); ?>" class="product-text-btn">View Details <i class="fas fa-arrow-right"></i></a>
+                                    echo ' <div class="product-date"><i class="far fa-clock"></i> ' . $time_diff . ' ago' . ' </div>'; // display the time elapsed
+                                    ?>
+                                </div>
+                                <div class="product-bottom">
+                                    <div class="product-price"><?php echo wc_price($product_price); ?></div>
+                                    <a href="<?php the_permalink(); ?>" class="product-text-btn">View Details <i class="fas fa-arrow-right"></i></a>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
 
-        <?php endwhile;
-        wp_reset_postdata();
-    else :
-        echo '<p>No products found</p>';
-    endif; ?>
-</div>
+                <?php endwhile;
+                wp_reset_postdata();
+            else :
+                echo '<p>No products found</p>';
+            endif; ?>
+        </div>
 
-<script>
-    jQuery(document).ready(function($) {
-        $('.product-favorite').on('click', function(e) {
-            e.preventDefault();
-            var productId = $(this).data('product-id');
-            var productName = $(this).data('product-name');
-            yith_wcwl_add_to_wishlist(productId, productName);
-        });
-    });
-</script>
-
-
-
+        <?php if (class_exists('YITH_WCWL')) : ?>
+            <script>
+                jQuery(document).ready(function($) {
+                    $('.product-favorite').on('click', function(e) {
+                        e.preventDefault();
+                        var productId = $(this).data('product-id');
+                        var productName = $(this).data('product-name');
+                        yith_wcwl_add_to_wishlist(productId, productName);
+                    });
+                });
+            </script>
+        <?php endif; ?>
     </div>
 </div>
 <!-- product area end -->
