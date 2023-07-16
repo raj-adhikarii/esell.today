a63a0ea1c1f60948668c9b0f4990d2d862f8547d
raj-adhikarii <rajadhikari777.ra@gmail.com>
2023-07-16 11:58:19 +0545

    wishlist added

--- front-page.php
+++ front-page.php
@@ -371,115 +371,119 @@ get_header();
         </div>
 
         <div class="row">
-            <?php
-            $args = array(
-                'post_type'      => 'product',
-                'posts_per_page' => 8,
-                'orderby'        => 'date',
-                'order'          => 'DESC',
-            );
+    <?php
+    $args = array(
+        'post_type'      => 'product',
+        'posts_per_page' => 8,
+        'orderby'        => 'date',
+        'order'          => 'DESC',
+    );
 
-            $products = new WP_Query($args);
+    $products = new WP_Query($args);
 
-            if ($products->have_posts()) :
-                while ($products->have_posts()) : $products->the_post();
-                    global $product;
-                    $product_id = $product->get_id();
-                    $product_name = $product->get_name();
-                    $product_price = $product->get_price();
-                    $product_sku = $product->get_sku();
-                    $product_image_url = wp_get_attachment_image_src(get_post_thumbnail_id($product_id), 'medium')[0];
-                    ?>
+    if ($products->have_posts()) :
+        while ($products->have_posts()) : $products->the_post();
+            global $product;
+            $product_id = $product->get_id();
+            $product_name = $product->get_name();
+            $product_price = $product->get_price();
+            $product_sku = $product->get_sku();
+            $product_image_url = wp_get_attachment_image_src(get_post_thumbnail_id($product_id), 'medium')[0];
+            ?>
 
-                    <div class="col-md-6 col-lg-3">
-                        <div class="product-item wow fadeInUp" data-wow-duration="1s" data-wow-delay=".25s">
-                            <div class="product-img">
-                                <a href="<?php the_permalink(); ?>">
-                                    <span class="product-status trending"><i class="fas fa-bolt-lightning"></i></span>
-                                    <?php if ($product_image_url) : ?>
-                                        <img src="<?php echo esc_url($product_image_url); ?>" alt="<?php echo esc_attr($product_name); ?>">
-                                    <?php endif; ?>
-                                    <a href="#" class="product-favorite" data-product-id="<?php echo get_the_ID(); ?>"><i class="far fa-heart"></i></a>
-                                </a>
+            <div class="col-md-6 col-lg-3">
+                <div class="product-item wow fadeInUp" data-wow-duration="1s" data-wow-delay=".25s">
+                    <div class="product-img">
+                        <a href="<?php the_permalink(); ?>">
+                            <span class="product-status trending"><i class="fas fa-bolt-lightning"></i></span>
+                            <?php if ($product_image_url) : ?>
+                                <img src="<?php echo esc_url($product_image_url); ?>" alt="<?php echo esc_attr($product_name); ?>">
+                            <?php endif; ?>
+                            <a href="#" class="product-favorite" data-product-id="<?php echo get_the_ID(); ?>"><i class="far fa-heart"></i></a>
+                        </a>
+                    </div>
+                    <div class="product-content">
+                        <div class="product-top">
+                            <div class="product-category">
+                                <div class="product-category-icon">
+                                    <i class="far fa-tv"></i>
+                                </div>
+                                <h6 class="product-category-title"><a href="#"><?php echo $product->get_categories(); ?></a></h6>
                             </div>
-                            <div class="product-content">
-                                <div class="product-top">
-                                    <div class="product-category">
-                                        <div class="product-category-icon">
-                                            <i class="far fa-tv"></i>
-                                        </div>
-                                        <h6 class="product-category-title"><a href="#"><?php echo $product->get_categories(); ?></a></h6>
-                                    </div>
-                                    <div class="product-rate">
-                                        <i class="fas fa-star"></i>
-                                        <span>4.5</span>
-                                    </div>
-                                </div>
-                                <div class="product-info">
-                                    <h5><a href="<?php the_permalink(); ?>"><?php echo $product_name; ?></a></h5>
-                                    <?php
-                                    $posted_time = get_the_time('U'); // get the post published time
-                                    $current_time = current_time('timestamp'); // get the current time
-
-                                    $time_diff = human_time_diff($posted_time, $current_time); // get the time elapsed since post was published
-
-                                    echo ' <div class="product-date"><i class="far fa-clock"></i> ' . $time_diff . ' ago' . ' </div>'; // display the time elapsed
-                                    ?>
-                                </div>
-                                <div class="product-bottom">
-                                    <div class="product-price"><?php echo wc_price($product_price); ?></div>
-                                    <a href="<?php the_permalink(); ?>" class="product-text-btn">View Details <i class="fas fa-arrow-right"></i></a>
-                                </div>
+                            <div class="product-rate">
+                                <i class="fas fa-star"></i>
+                                <span>4.5</span>
                             </div>
                         </div>
+                        <div class="product-info">
+                            <h5><a href="<?php the_permalink(); ?>"><?php echo $product_name; ?></a></h5>
+                            <?php
+                            $posted_time = get_the_time('U'); // get the post published time
+                            $current_time = current_time('timestamp'); // get the current time
+
+                            $time_diff = human_time_diff($posted_time, $current_time); // get the time elapsed since post was published
+
+                            echo ' <div class="product-date"><i class="far fa-clock"></i> ' . $time_diff . ' ago' . ' </div>'; // display the time elapsed
+                            ?>
+                        </div>
+                        <div class="product-bottom">
+                            <div class="product-price"><?php echo wc_price($product_price); ?></div>
+                            <a href="<?php the_permalink(); ?>" class="product-text-btn">View Details <i class="fas fa-arrow-right"></i></a>
+                        </div>
                     </div>
+                </div>
+            </div>
 
-                <?php endwhile;
-                wp_reset_postdata();
-            else :
-                echo '<p>No products found</p>';
-            endif; ?>
-        </div>
+        <?php endwhile;
+        wp_reset_postdata();
+    else :
+        echo '<p>No products found</p>';
+    endif; ?>
+</div>
 
-        <script>
-            jQuery(document).ready(function($) {
-                $('.product-favorite').on('click', function(e) {
-                    e.preventDefault();
-                    var productId = $(this).data('product-id');
-                    var data = {
-                        action: 'add_to_wishlist',
-                        product_id: productId,
-                    };
-                    $.ajax({
-                        type: 'POST',
-                        dataType: 'json',
-                        url: '<?php echo admin_url('admin-ajax.php'); ?>',
-                        data: data,
-                        beforeSend: function() {
-                            // Show loading spinner or any UI indication
-                        },
-                        success: function(response) {
-                            if (response.success) {
-                                // Wishlist addition successful
-                                // You can add your own success message or perform any other action
-                                console.log('Product added to wishlist');
-                            } else {
-                                // Wishlist addition failed
-                                // You can add your own error message or perform any other action
-                                console.log('Failed to add product to wishlist');
-                            }
-                        },
-                        error: function(error) {
-                            // Error handling
-                            console.log('AJAX error:', error);
-                        },
-                        complete: function() {
-                            // Hide loading spinner or any UI indication
-                        }
-                    });
-                });
+<script>
+    jQuery(document).ready(function($) {
+        $('.product-favorite').on('click', function(e) {
+            e.preventDefault();
+            var productId = $(this).data('product-id');
+            var data = {
+                action: 'add_to_wishlist',
+                product_id: productId,
+            };
+            $.ajax({
+                type: 'POST',
+                dataType: 'json',
+                url: '<?php echo admin_url('admin-ajax.php'); ?>',
+                data: data,
+                beforeSend: function() {
+                    // Show loading spinner or any UI indication
+                },
+                success: function(response) {
+                    if (response.success) {
+                        // Wishlist addition successful
+                        // You can add your own success message or perform any other action
+                        console.log('Product added to wishlist');
+                        alert('Product added to wishlist successfully!');
+                    } else {
+                        // Wishlist addition failed
+                        // You can add your own error message or perform any other action
+                        console.log('Failed to add product to wishlist');
+                        alert('Failed to add product to wishlist!');
+                    }
+                },
+                error: function(error) {
+                    // Error handling
+                    console.log('AJAX error:', error);
+                    alert('AJAX error occurred!');
+                },
+                complete: function() {
+                    // Hide loading spinner or any UI indication
+                }
             });
-        </script>
+        });
+    });
+</script>
+
 
     </div>
 </div>
